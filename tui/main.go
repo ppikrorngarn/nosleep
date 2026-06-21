b6ab94bdcc0fb121bd08f7f4341118b7e1675eb5
Piyawasin Pikror-ngarn <piyawasin@gmail.com>
2026-06-21 09:44:34 +0700

    Add main entry point

--- tui/main.go
+++ tui/main.go
@@ -208,3 +208,11 @@ func getBinaryPath() string {
 	binaryPath, _ := os.Executable()
 	return binaryPath
 }
+
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
